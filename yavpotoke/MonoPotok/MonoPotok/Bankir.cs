34369ec7ceb393da9306b650487f5284fc244779
Lisitsin <113295347+lisenok-lisitsin@users.noreply.github.com>
2023-12-02 16:25:01 +0300

    Add files via upload

---
+++ yavpotoke/MonoPotok/MonoPotok/Bankir.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoPotok
+{
+    public class Bankir
+    {
+        public static string FIO_my { get; set; }
+        public static int Schet_my { get; set; }
+        public static int Balance_my { get; set; }
+        public static int Balance_sm { get; set; }
+
+        public void Add(int bal)
+        {
+            Balance_my = Balance_my + bal;
+        }
+        public void Rem(int bal)
+        {
+            Balance_my = Balance_my - bal;
+        }
+        public void Wassup()
+        {
+            Console.WriteLine(Balance_my);
+        }
+        public void Give(int mon)
+        {
+            if (mon > Balance_my)
+            {
+                Balance_my = Balance_my - mon;
+                Balance_sm = Balance_sm + mon;
+            }
+            else
+            {
+                Console.WriteLine("Not enough money");
+            }
+        }
+        public void ShowMe()
+        {
+            Console.WriteLine($"\nФИО: {FIO_my}\nНомер счёта: {Schet_my}\nБаланс: {Balance_my}");
+        }
+    }
+}
---
+++ yavpotoke/MonoPotok/MonoPotok/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonoPotok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread printing = new Thread(Skoll);
+            //printing.Start();
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine($"{i}> Прыг~");
+            //    Thread.Sleep(230);
+            //}
+            //void Skoll()
+            //{
+            //    for (int i = 0; i < 6; i++)
+            //    {
+            //        Console.WriteLine($"{i}> Скок!");
+            //        Thread.Sleep(230);
+            //    }
+            //}
+            //Console.ReadKey();
+
+            Bankir.Balance_my = 0;
+            Bankir.Balance_sm = 0;
+            Bankir.FIO_my = "Владислав Фуфлыжников";
+            Bankir.Schet_my = 323232332;
+            Bankir bank = new Bankir();
+            bank.Add(10000);
+            bank.Wassup();
+            bank.ShowMe();
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("=========Банкомат========");
+            Console.WriteLine("=========================");
+            Console.WriteLine("\n1 - Узнать всю инфу\n2 - Узнать счёт\n3 - Внести денег\n4 - Снять денег\n5 - Перевести денег");
+            switch (Console.ReadLine())
+            {
+                case 
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
